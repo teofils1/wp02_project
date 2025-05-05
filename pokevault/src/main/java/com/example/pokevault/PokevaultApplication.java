435558cc4f05ef2f6318b67626cf08115af7c998
teofils1 <teodormarinescu84@gmail.com>
2025-05-05 23:16:25 +0300

    first commit

---
+++ pokevault/src/main/java/com/example/pokevault/PokevaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokevaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokevaultApplication.class, args);
+	}
+
+}
