81c20885a2fb54d9a7c72d5d53cf8985a4979d8a
teofils1 <teodormarinescu84@gmail.com>
2025-05-05 17:44:39 +0300

    first commit

---
+++ pokevault/src/test/java/com/example/pokevault/PokevaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokevault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokevaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
